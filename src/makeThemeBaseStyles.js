12741b74fe7868b0309111fe9ccbc7856e01bd48
johnwatkins0 <johnwatkins0@gmail.com>
2018-01-02 17:36:21 -0500

    Resolves #1 add additional features

--- src/makeThemeBaseStyles.js
+++ src/makeThemeBaseStyles.js
@@ -1,6 +1,11 @@
-export const makeThemeBaseStyles = themes => `.${themes
-    .map(theme => theme.name)
-    .join(', .')} {
+export const makeThemeBaseStyles = themes =>
+    `.${themes.map(theme => theme.name).join(', .')} {
   background-color: var(--background-color);
   color: var(--color);
-}`;
+  border-color: var(--border-color);
+}
+` +
+    `.${themes.map(theme => `${theme.name}::hover`).join(', .')} {
+  background-color: var(--background-hover-color);
+}
+`;
