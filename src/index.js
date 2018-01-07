11d74e7872e431c65c293e7596f12715056aaed6
johnwatkins0 <johnwatkins0@gmail.com>
2018-01-07 16:41:11 -0500

    Remove check for at rule at start of execution because it was causing problems

--- src/index.js
+++ src/index.js
@@ -1,53 +1,47 @@
-import postcss from "postcss"
-import defaultOpts from "../optsDefaults"
-import {validateOpts} from "./validateOpts"
-import {makeRoot} from "./makeRoot"
-import {makeDefaultStyles} from "./makeDefaultStyles"
-import {makeThemeBaseStyles} from "./makeThemeBaseStyles"
-import {getDefaultTheme} from "./utils/getDefaultTheme"
-import {addMissingProps} from "./utils/addMissingProps"
-import {makeThemes} from "./makeThemes"
+import postcss from 'postcss';
+import defaultOpts from '../optsDefaults';
+import { validateOpts } from './validateOpts';
+import { makeRoot } from './makeRoot';
+import { makeDefaultStyles } from './makeDefaultStyles';
+import { makeThemeBaseStyles } from './makeThemeBaseStyles';
+import { getDefaultTheme } from './utils/getDefaultTheme';
+import { addMissingProps } from './utils/addMissingProps';
+import { makeThemes } from './makeThemes';
 
 const buildCSS = pluginOpts =>
     new Promise(async (resolve, reject) => {
         try {
-            const opts = await validateOpts(pluginOpts)
+            const opts = await validateOpts(pluginOpts);
             const defaultTheme = addMissingProps({
                 theme: getDefaultTheme(opts),
                 opts
-            })
+            });
 
             resolve(`
 ${makeRoot(defaultTheme)}
 ${makeDefaultStyles(defaultTheme)}
 ${makeThemeBaseStyles(opts.themes)}
-${makeThemes({opts, themes: opts.themes})}
-`)
+${makeThemes({ opts, themes: opts.themes })}
+`);
         } catch (err) {
-            reject(err)
+            reject(err);
         }
-    })
+    });
 
-const customPropsThemes = (opts = {}) => (root, result) =>
+const customPropsThemes = (opts = {}) => root =>
     new Promise((resolve, reject) => {
-        if (root.source.input.css.indexOf("@custom-props-themes") === -1) {
-            result.warn("@custom-props-themes at rule not found")
-            resolve()
-            return
-        }
-
-        root.walkAtRules("custom-props-themes", async rule => {
+        root.walkAtRules('custom-props-themes', async rule => {
             try {
                 const css = await buildCSS(
                     Object.keys(opts).length ? opts : defaultOpts
-                )
-                rule.before(css)
-                rule.remove()
-                resolve()
+                );
+                rule.before(css);
+                rule.remove();
+                resolve();
             } catch (err) {
-                reject(err)
+                reject(err);
             }
-        })
-    })
+        });
+    });
 
-module.exports = postcss.plugin("custom-props-themes", customPropsThemes)
+module.exports = postcss.plugin('custom-props-themes', customPropsThemes);
